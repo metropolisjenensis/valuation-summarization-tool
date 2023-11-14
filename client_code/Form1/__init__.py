240900c8ad87c04a7823eda0c6ec141d8e9b9997
konabe@deloitte.de <konabe@deloitte.de>
2023-11-14 14:52:32 +0000

    Created myicon.png, Form1.create_Powerpoint and Form1.summarization; Edited settings, theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+from . import create_Powerpoint
+from . import summarization
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +10,23 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    self.label_1.text=str(file.name )
+    pass
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.query_holen()
+    #summarization.generate_summary_kivy(str(file.name),self.query_holen())
+
+  def query_holen(self):
+    if self.radio_button_1.selected==True:
+      return str(self.radio_button_1.text)
+    elif self.radio_button_2.selected==True:
+      return str(self.radio_button_2.text)
+    elif self.radio_button_3.selected==True:
+      return str(self.radio_button_3.text)
+    else:
+      return print("No Query selected")
+    
