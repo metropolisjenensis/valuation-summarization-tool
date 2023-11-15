7abfd552784a0a6c505940b7888199e01173a041
konabe@deloitte.de <konabe@deloitte.de>
2023-11-15 14:55:03 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -18,7 +18,7 @@ class Form1(Form1Template):
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.x=anvil.server.call("hi",self.label_1.text,self.query_holen())
-    self.text_area_1=str(self.x["answer"])
+    self.text_area_1.text=str(self.x["answer"])
     #summarization.generate_summary_kivy(str(file.name),self.query_holen())
 
   def query_holen(self):
